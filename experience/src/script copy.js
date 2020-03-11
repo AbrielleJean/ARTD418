28e10d21adafe15c8faad330076501ab81cd5c31
AbrielleJean <AbrielleJean@users.noreply.github.com>
2020-03-10 23:02:37 -0500

    More updates
    updating things for class

--- experience/src/script copy.js
+++ experience/src/script copy.js
@@ -8,43 +8,43 @@ function setup() {
 
     // retrieve all the SVG paths in the html
     var pathEls = document.querySelectorAll('path');
-	var stroke = document.querySelectorAll('.st6');
+//	var stroke = document.querySelectorAll('.st6');
 
     for (var i = 0; i < pathEls.length; i++) {
 
         // for each path
         var pathEl = pathEls[i];
-		var stroke = stroke[i];
+	//	var stroke = stroke[i];
 
         // Set an offset along the path.
         var offset = anime.setDashoffset(pathEl);
-		var offset = anime.setDashoffset(stroke);
+	//	var offset = anime.setDashoffset(stroke);
 		
 
         pathEl.setAttribute('stroke-dashoffset', offset);
 		
 		
-		let tmp = anime({
-			targets: stroke,
-			translateX: 1250,
-			direction: 'alternate',
-			loop: 'false',
-			easing: 'easeInSine'
+//		let tmp = anime({
+//			targets: stroke,
+//			translateX: 1250,
+//			direction: 'alternate',
+//			loop: 'false',
+//			easing: 'easeInSine'
 			
-		})
+//		})
 	
 
         // Animation parameters
-  //      let tmp = anime({
-  //        targets: pathEl,
-  //        strokeDashoffset: [offset, 0],
-  //        duration: anime.random(1000, 3000),
-  //        delay: anime.random(0, 3000),
-  //        loop: true,
-  //        direction: 'alternate',
-  //        easing: 'easeInOutSine',
-  //        autoplay: false
-  //      });
+        let tmp = anime({
+          targets: pathEls,
+          strokeDashoffset: [offset, 0],
+          duration: anime.random(1000, 3000),
+          delay: anime.random(0, 3000),
+          loop: true,
+          direction: 'alternate',
+          easing: 'easeInOutSine',
+          autoplay: false
+        });
 
         tmp.play()
             // storage of animated pats in a collection
@@ -53,9 +53,9 @@ function setup() {
 
 }
 
-function mouseWheel (event)
-{
-	print(event.Delta);
-	pos += event.Delta
-	
-}
+//function myFunction() {
+//    var str= "You're a loser.";
+//    var res= str.split(".");
+//    document.getElementsById("text").innerHTML = res;
+    
+//}
