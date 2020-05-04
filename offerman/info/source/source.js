48535021e4864950f9d38baca8cf33170d985c99
AbrielleJean <abujack2@illinois.edu>
2020-05-04 02:14:43 -0500

    So many things
    fixed slideshow on page 1. added sticky nav. added illustration and animation.

--- offerman/info/source/source.js
+++ offerman/info/source/source.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 
+anime ({
+	targets: '.glass-1',
+	rotate: 15,
+	easing: 'easeInOutSine',
+	direction: 'alternate',
+	loop: true,
+})
+
+anime ({
+	targets: '.glass-2',
+	rotate: -15,
+	easing: 'easeInOutSine',
+	direction: 'alternate',
+	loop: true,
+})
+
